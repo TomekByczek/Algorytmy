410835fac7625f2a7f9751f563a6388884822c67
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-02 18:26:45 +0100

    Usunięte  śmieci

--- maxElement/script.js
+++ maxElement/script.js
@@ -1,12 +1,4 @@
 function maxElement(table) {
-    // for (i = 0; i < table.length; i++) {
-    //     if (table[i] < table[0]) {
-    //         var max = table[0]
-    //     }
-    //     else {
-    //         max = table[0]
-    //     }
-    // }
     //table.sort((a, b) => a - b)
     //return table[table.length]
     var max = table[0];
