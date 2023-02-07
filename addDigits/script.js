6421c7b4945eb03403e16f8df8db6ab00eb1fee3
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-07 21:24:30 +0100

    chartAt

--- addDigits/script.js
+++
@@ -1,9 +0,0 @@
-function addStringChar(number) {
-    var num = []
-    num.push(number + '')
-    var add = 0
-    for (var i = 0; i < num.length; i++) {
-        add = add + (num.charAt(i))
-    }
-    return add
-}
