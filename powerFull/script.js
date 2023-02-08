47e0a447d59c6f00437d08d94476f1ae39f52f9c
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-08 21:00:11 +0100

    powerFull

---
+++ powerFull/script.js
@@ -0,0 +1,16 @@
+function powerFull(base, index) {
+    var result = base;
+    if (index == 0) {
+        result = 1;
+    }
+    if (index == 1) {
+        result = base;
+    }
+    if (index < 0) {
+        base = 1 / base; result = base; index = index * -1;
+    }
+    for (var i = 0; i < index - 1; i++) {
+        result = result * base
+    }
+    return result
+}
