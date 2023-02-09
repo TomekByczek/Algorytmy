30f6efa2a009625e96127c90349cad7fe4f56508
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-09 19:36:37 +0100

    Poprawiony addDigits

--- addDigits/script.js
+++ addDigits/script.js
@@ -1,8 +1,14 @@
 function sumDigits(number) {
-    var num = number + '';
-    var sum = 0;
-    for (var i = 0; i < num.length; i++) {
-        sum = sum + +(num.charAt(i))
+    if (number < 0) {
+        number = number * -1;
     }
-    return sum
+    if ((number % 1) == 0) {
+        var num = number + '';
+        var sum = 0;
+        for (var i = 0; i < num.length; i++) {
+            sum = sum + +(num.charAt(i));
+        }
+        return sum;
+    }
+    return 'To nie jest liczba całkowita !!!';
 }
