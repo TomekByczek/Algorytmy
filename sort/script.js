7d90db88648caf30dc01c240b84b3eac387b07ab
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-16 17:49:09 +0100

    optimal sort

--- sort/script.js
+++ sort/script.js
@@ -1,16 +1,20 @@
 function sortTable(table) {
+    var jump = 1;
     do {
+        if (swap === true) {
+            jump = jump + 1;
+        }
         var swap = false;
-        for (var i = 0; i < table.length - 1; i++) {
+        for (var i = 0; i < table.length - jump; i++) {
             if (table[i] > table[i + 1]) {
                 var a = table[i];
                 table[i] = table[i + 1];
                 table[i + 1] = a;
                 swap = true;
             }
-            else {
-                continue;
-            }
+            //else {
+            //continue;
+            //}
         }
 
     } while (swap === true)
