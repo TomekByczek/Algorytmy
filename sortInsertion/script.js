040d5c6f39691c4654bcf5e6ec95a817c2fb1249
Tomek Byczek <tomek.byczek1968@gmail.com>
2023-02-18 17:55:56 +0100

    Wstawianie skończone?

--- sortInsertion/script.js
+++ sortInsertion/script.js
@@ -1,16 +1,27 @@
+
 function sortInsertion(myTable) {
-    var aux = []
-    var tableSorting = [myTable[0]];
-    for (var i = 1; i < myTable.length; i++) {
-        for (var j = 0; j < tableSorting.length; j++) {
-            if (myTable[i] >= tableSorting[j]) {
-                tableSorting[j + 1] = myTable[i];
+    var tableSorting = [];
+    var length = myTable.length;
+    for (var i = 0; i < length; i++) {
+        var temp = myTable.pop();
+        var insert = false;
+        var tabLength = tableSorting.length;
+        for (var j = 0; j < tabLength; j++) {
+            if (temp <= tableSorting[j]) {
+                for (var k = tabLength - 1; k >= j; k--) {
+                    tableSorting[k + 1] = tableSorting[k];
+                }
+                tableSorting[j] = temp;
+                insert = true;
+                break
             }
-            else {
-                aux[0] = myTable[i];
-                tableSorting = aux.concat(tableSorting)
-            }
+
+
+
+        }
+        if (insert != true) {
+            tableSorting.push(temp);
         }
     }
-    return tableSorting
+    return tableSorting;
 }
