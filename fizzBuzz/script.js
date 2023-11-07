2592112cd734894728fd9ec3c0eb0effd9a5715b
Tomasz Byczek <tomek.byczek1968@gmail.com>
2023-11-07 19:09:08 +0100

    fizzBuzz

---
+++ fizzBuzz/script.js
@@ -0,0 +1,14 @@
+function fizzBuzz (a, b) {
+    var result = ""
+    for (var i=a; i <= b; i++){ // asynchroniczność
+        if (i % 3 == 0 && i % 5 == 0)
+        result = result + " " + "FizzBuzz";
+        else if (i % 3 == 0)
+        result = result + " " + "Fizz";
+        else if (i % 5 == 0)
+        result = result + " " + "Buzz";
+        else
+        result = result + " "  + i
+    }
+    return result
+}
