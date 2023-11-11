e4ff414212ee9d3adc17df0f84dfb07f6f6981e3
Tomasz Byczek <tomek.byczek1968@gmail.com>
2023-11-11 22:40:06 +0100

    następny krok

--- fizzBuzz/script.js
+++ fizzBuzz/script.js
@@ -1,14 +1,19 @@
 function fizzBuzz (a, b) {
-    var result = ""
+   // var result = ""
     for (var i=a; i <= b; i++){ // asynchroniczność
-        if (i % 3 == 0 && i % 5 == 0)
+        if (i % 3 == 0 && i % 5 == 0){
         result = result + " " + "FizzBuzz";
-        else if (i % 3 == 0)
+        }
+        else if (i % 3 == 0){
         result = result + " " + "Fizz";
-        else if (i % 5 == 0)
+        }
+        else if (i % 5 == 0){
         result = result + " " + "Buzz";
-        else
-        result = result + " "  + i
+        }
+        else{
+        result = result + " "  + i;
+        console.log ("dupa");
+        }
     }
     return result
 }
